068f966a412d0a82c68b1d46da0c02fa019d70c9
Robert Campbell <rrc@robert-campbell.com>
2011-11-27 14:56:16 +0100

    added chapter 1, section 1 exercises

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject sicp "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "Structure and Interpretation of Computer Programs in Clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
